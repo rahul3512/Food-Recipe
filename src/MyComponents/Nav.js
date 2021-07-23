9577f8b247968d1c75df45054a99391eefa1fa25
Rahul Kumar <rahulkumarpatna53@gmail.com>
2021-07-23 21:52:12 +0530

    adding navbar and some styling

---
+++ src/MyComponents/Nav.js
@@ -0,0 +1,129 @@
+import React , {useState , useEffect} from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import {Menu as MenuIcon , Search as SearchIcon} from '@material-ui/icons';
+import FoodLogo from '../Assets/FoodLogo.jpg'
+
+
+
+   
+        
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+
+function Nav({ parentCallback }) {
+    
+    const [search, setSearch] = useState("Chicken");
+    const [see, setSee] = useState('Chicken');
+    const classes = useStyles();
+    
+    function send(e) {
+        
+        if (e.code == 'Enter') {
+            setSee(search);
+            parentCallback(search);
+        }
+    }
+
+    useEffect(() => {
+        
+        parentCallback(search);
+    } , [])
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <img src={FoodLogo} alt="Icon" width="35px" height="30px" />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        Food Recipe
+                    </Typography>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        {`Showing result for  '${see}'`}
+                    </Typography>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            onKeyPress= {(e) =>send(e)}
+                            onChange={(e) => { setSearch(e.target.value);  }}
+                            value = {search}
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+export default Nav
--- src/MyComponents/Recipe.js
+++ src/MyComponents/Recipe.js
@@ -5,11 +5,11 @@ export const Recipe = (props) => {
   return (
     <div className="main-content">
       <div className="content">
-        <h2>{props.title}</h2>
+        <h1>{props.title}</h1>
         <p>Calories : {props.calorie}</p>
         <img src={props.src} alt="Image coming" className="img" />
       </div>
-      <div>
+      <div className="ingredient">
         <h2>Ingredients</h2>
         <ul>
           {props.ingredients.map((ingrad) => (
